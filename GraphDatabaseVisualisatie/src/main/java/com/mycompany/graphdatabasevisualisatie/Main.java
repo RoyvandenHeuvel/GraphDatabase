9950a7b03d67246f09ef85199380704dd2205db7
RoyvandenHeuvel <0878963@hr.nl>
2016-06-25 15:40:52 +0200

    init

---
+++ GraphDatabaseVisualisatie/src/main/java/com/mycompany/graphdatabasevisualisatie/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.graphdatabasevisualisatie;
+
+import processing.core.PApplet;
+
+/**
+ *
+ * @author Roy van den Heuvel
+ */
+public class Main extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main(new String[]{Main.class.getName()});
+
+    }
+}
